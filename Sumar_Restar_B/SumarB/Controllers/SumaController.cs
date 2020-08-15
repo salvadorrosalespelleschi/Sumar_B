dadd901beb214e9ca4ba01902f60f9788f8a5660
Salvador Rosales Pelleschi <a2018114271@estudiantes.upsa.edu.bo>
2020-08-14 20:47:31 -0400

    CLASE 14/8/2020:AÑADIMOS FUNCION DE MULTIPLICACION

--- Sumar_Restar_B/SumarB/Controllers/SumaController.cs
+++ Sumar_Restar_B/SumarB/Controllers/SumaController.cs
@@ -1,24 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
+﻿
 namespace SumarB.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
     [Route("api/[controller]")]
     [ApiController]
     public class SumaController : ControllerBase
     {
         [HttpGet]
+        [Route("Add")]
         public int Add(int a, int b){
             return a+b;       
         }
+        [HttpGet]
+        [Route("Multiply")]
+        public int Mul(int a, int b)
+        {   
+            return a * b;
+        }
         [HttpPost]
+        [Route("Add")]
         public int Add2([FromHeader]int a, [FromHeader] int b)
         {
             return a + b;
+        }
+        [HttpPost]
+        [Route("Multiply")]
+        public int Mul2([FromHeader] int a, [FromHeader] int b) {
+            return a * b;
         }
     }
 }
